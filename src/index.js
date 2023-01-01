5a8cd739e3afecf931277ff418801af4e1ade7a9
RuslanMiki <mikitchenko_ruslan@ukr.net>
2023-01-01 22:46:24 +0100

    modal team upd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/key-word-search';
 import { fetchTrendingMovies } from './js/fetch-movies';
-
+import './js/team-modal'
 fetchTrendingMovies().then(data => {
   console.log(data.results);
   return data.results;
