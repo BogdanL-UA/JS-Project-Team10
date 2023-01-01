805b8091d870063c04f5d3e73675d07209ae2d62
Bogdan Lytvynyuk <103260029+BogdanL-UA@users.noreply.github.com>
2023-01-02 00:14:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import './js/createPageHome';
 import './js/pagination';
 import './js/key-word-search';
-import { fetchTrendingMovies } from './js/fetch-movies';
 import './js/team-modal'
-fetchTrendingMovies().then(data => {
-  console.log(data.results);
-  return data.results;
-});
 
 import './js/get-trend-movies';
