8dc4b34023ecb220fd218c07d7bb1082b871f3be
rasengun <69122511+rasengun@users.noreply.github.com>
2023-01-01 15:10:49 +0200

    fix

--- src/js/createSearchGallery.js
+++ src/js/createSearchGallery.js
@@ -1,46 +1,28 @@
 import { searchGenres, movieId } from './genres';
 import createGenresMarkup from './create-genres-markup';
-
-
-
-
 // searchGenres();
-
 
 // let getNewId = movieId
 //   .filter(genre => genre_ids.includes(genre.id))
 //   .map(genre => genre.name)
 //   .join(', ');
 
+const noPosterImg =
+  'https://freedesignfile.com/upload/2014/07/Movie-time-design-elements-vector-backgrounds-01.jpg';
+const fileSize = `w500`;
+const basePosterUrl = 'https://image.tmdb.org/t/p/';
 
-export function createGallery(film) {
-  searchGenres();
-
-  let getNewId = movieId
-    .filter(genre => genre_ids.includes(genre.id))
-    .map(genre => genre.name)
-    .join(', ');
-
-
-  const noPosterImg =
-    'https://freedesignfile.com/upload/2014/07/Movie-time-design-elements-vector-backgrounds-01.jpg';
-  const fileSize = `w500`;
-  const basePosterUrl = 'https://image.tmdb.org/t/p/';
-
-  function generatePosterImgLink(poster_path) {
-    if (poster_path === null) {
-      return noPosterImg;
-    }
-    return `${basePosterUrl}${fileSize}${poster_path}`;
+function generatePosterImgLink(poster_path) {
+  if (poster_path === null) {
+    return noPosterImg;
   }
+  return `${basePosterUrl}${fileSize}${poster_path}`;
 }
 
-export function createGallery2(film) {
-
+export function createGallery(film) {
   return film
     .map(({ id, genre_ids, original_title, poster_path, release_date }) => {
       const releaseYear = release_date.slice(0, 4);
-      const posterUrl = 'https://image.tmdb.org/t/p/w500/' + poster_path;
 
       const genresMarkup = createGenresMarkup(genre_ids);
 
@@ -57,6 +39,4 @@ export function createGallery2(film) {
         </li>`;
     })
     .join('');
-  
- 
 }
