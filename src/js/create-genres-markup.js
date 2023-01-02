4f5da93b47b157f542a3114102e56011bc3fcd9a
Bogdan Lytvynyuk <103260029+BogdanL-UA@users.noreply.github.com>
2023-01-02 11:46:10 +0200

    Update create-genres-markup.js

--- src/js/create-genres-markup.js
+++ src/js/create-genres-markup.js
@@ -21,6 +21,10 @@ const genresList = [
 ];
 
 export default function createGenresMarkup(genre_ids) {
+  console.log(genre_ids);
+  if (genre_ids.length === 0) {
+    return 'Genre is unknown';
+  }
   const convertedGenres = [];
   const genresMarkup = [];
   for (const genre_id of genre_ids) {
