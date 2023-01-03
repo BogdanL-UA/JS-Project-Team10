e5e955e202072ff09e19b64985a1e30f19daf3d3
rasengun <69122511+rasengun@users.noreply.github.com>
2023-01-03 13:09:11 +0200

    search

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,10 +4,7 @@ import { refs } from './refs';
 import { FilmsApiService } from './apiService';
 import { createMovieCard } from './get-trend-movies';
 
- 
-
 const filmsApiService = new FilmsApiService();
-
 
 function pagination() {
   filmsApiService.page = 1;
@@ -28,30 +25,6 @@ function pagination() {
       refs.filmsGallery.innerHTML = '';
       createMovieCard(films);
     });
-    
   });
 }
 export { pagination };
-
-  function paginationOnQuery() {
-  filmsApiService.page = 1;
-  const options = {
-    totalItems: FilmsApiService.totalPages,
-    itemsPerPage: 20,
-    visiblePages: 5,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-  };
-
-  const pagination = new Pagination(refs.pagination, options);
-  pagination.reset();
-  pagination.on('beforeMove', function (eventData) {
-    filmsApiService.page = eventData.page;
-    filmsApiService.getFilmsByQuery().then(films => {
-      refs.filmsGallery.innerHTML = '';
-      createGallery(film);
-    });
-  });
-}
-export { paginationOnQuery };
