c901583b8f82ab40ca4bd8b81ed168aafa22e7c3
Svetlana210 <svetik.may24@gmail.com>
2023-01-04 17:11:00 +0100

    add dark-theme

--- src/indexHome.js
+++ src/indexHome.js
@@ -5,6 +5,7 @@ import './js/pagination';
 
 import './js/get-trend-movies';
 import './js/team-modal';
+import './js/dark-theme';
 // import './js/scroll';
 import topUp from './js/top';
 
--- src/indexLibrary.js
+++ src/indexLibrary.js
@@ -2,6 +2,7 @@ import './js/createPageHome';
 import './js/createSearchGallery';
 import './js/pagination';
 import './js/team-modal';
+import './js/dark-theme';
 // import './js/scroll';
 import topUp from './js/top';
 
