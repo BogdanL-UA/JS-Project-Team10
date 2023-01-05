b9e770dc157cde85a512bb7a928265ef68c09a75
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2023-01-05 10:05:06 +0200

    add

--- src/index-library.js
+++ src/index-library.js
@@ -1,11 +1,11 @@
 // import './js/createPageHome';
 // import './js/createSearchGallery';
 
-// import './js/pagination';
+import './js/pagination';
 
-// import './js/team-modal';
+import './js/team-modal';
 import './js/dark-theme';
 // import './js/scroll';
 import topUp from './js/top';
 
-topUp();
+// topUp();
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export const refs = {
   searchForm: document.querySelector('.form'),
-  filmsGallery: document.querySelector('.movies__list'),
+  // filmsGallery: document.querySelector('.movies__list'),
   gallery: document.querySelector('.movies__list'),
   pagination: document.getElementById('tui-pagination-container'),
   movieCard: document.querySelector('.movies__item'),
