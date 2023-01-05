c49e9fb96c8a96eb945c5c26232cd6c3a954cb05
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-05 12:29:53 +0100

    remove conflict

--- src/indexLibrary.js
+++ src/indexLibrary.js
@@ -1,11 +1,9 @@
 // import './js/createPageHome';
 // import './js/createSearchGallery';
 // import './js/pagination';
-import './js/library/on-library-load';
 import './js/team-modal';
 import './js/dark-theme';
 // import './js/scroll';
 import topUp from './js/top';
 
 topUp();
-
