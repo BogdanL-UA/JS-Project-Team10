6077a86f11975d4b8896fd008784cefaac675105
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-05 12:09:21 +0100

    on library load

--- src/indexLibrary.js
+++ src/indexLibrary.js
@@ -1,9 +1,11 @@
 // import './js/createPageHome';
 // import './js/createSearchGallery';
 // import './js/pagination';
+import './js/library/on-library-load';
 import './js/team-modal';
 import './js/dark-theme';
 // import './js/scroll';
 import topUp from './js/top';
 
 topUp();
+
