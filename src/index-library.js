06182ab3e00edefe0530b06384f5ee6e23be3e73
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-05 12:37:29 +0100

    resolve conflict

---
+++ src/index-library.js
@@ -0,0 +1,11 @@
+// import './js/create-page-home';
+// import './js/create-search-gallery';
+
+import './js/pagination';
+
+import './js/team-modal';
+import './js/dark-theme';
+// import './js/scroll';
+import topUp from './js/top';
+
+// topUp();
--- src/indexLibrary.js
+++
@@ -1,9 +0,0 @@
-// import './js/createPageHome';
-// import './js/createSearchGallery';
-// import './js/pagination';
-import './js/team-modal';
-import './js/dark-theme';
-// import './js/scroll';
-import topUp from './js/top';
-
-topUp();
