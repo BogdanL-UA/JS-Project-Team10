44ee37a9174b95d952971d6d3d21d673e053074f
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-07 18:58:26 +0100

    library fix

--- src/js/library/on-library-load.js
+++ src/js/library/on-library-load.js
@@ -2,42 +2,45 @@ import { renderLibrary } from './render-library';
 import { refs } from '../refs';
 import { activateWatchedBtn, activateQueueBtn, deactivateWatchedBtn, deactivateQueueBtn } from './library-buttons-functions';
 import { clearLibraryContainer } from './clear-container';
-import { getFromLocalStorage } from './get-from-local-storage';
+import { checkWatched, checkQueue } from './get-from-local-storage';
 
+// let watchedMovies = getFromLocalStorage('watched');
+// let queueMovies = getFromLocalStorage('queue');
 
-
-let watchedMovies = getFromLocalStorage('watched');
-let queueMovies = getFromLocalStorage('queue');
+let watchedMovies = checkWatched();
+let queueMovies = checkQueue();
 
 //перевірка local storage при завантаженні сторінки
 if (
   (watchedMovies == null || watchedMovies.length === 0) &&
   (queueMovies == null || queueMovies.length === 0)
 ) {
-  //в localstorage нічого немає
-  refs.message.innerHTML = '<p>Ви ще не додали фільми до бібліотеки</p>';
-   refs.library.innerHTML = '';
+    //в localstorage нічого немає
+    refs.message.innerHTML = '<p>Library is empty</p>';
+    refs.library.innerHTML = '';
 } else if (watchedMovies.length > 0) {
-  //в localstorage є переглянуті фільми (пріоритет)
-  activateWatchedBtn();
-  renderLibrary(watchedMovies);
+    //в localstorage є переглянуті фільми (пріоритет)
+    activateWatchedBtn();
+    renderLibrary(watchedMovies);
 } else if (queueMovies.length > 0) {
-  //в localstorage є фільми в черзі
-  activateQueueBtn();
-  renderLibrary(queueMovies);
+    //в localstorage є фільми в черзі
+    activateQueueBtn();
+    renderLibrary(queueMovies);
 }
 
+// колбеки
 const onWatchedClick = (event) => {
 
   deactivateQueueBtn();
   activateWatchedBtn();
   clearLibraryContainer();
 
-  watchedMovies = getFromLocalStorage('watched');
+  // watchedMovies = getFromLocalStorage('watched');
+  watchedMovies = checkWatched();
 
   if (watchedMovies == null || watchedMovies.length === 0) {
     refs.message.innerHTML =
-      '<p>Ви ще не додали фільми до списку переглянутих</p>';
+      '<p>List of watches films is empty</p>';
     refs.library.innerHTML = '';
     refs.pagination.style.display = 'none';
   } else if (watchedMovies.length > 0) {
@@ -53,18 +56,18 @@ const onQueueClick = (event) => {
   deactivateWatchedBtn();
   clearLibraryContainer();
 
-  queueMovies = getFromLocalStorage('queue');
+  // queueMovies = getFromLocalStorage('queue');
+  queueMovies = checkQueue();
 
   if (queueMovies == null || queueMovies.length === 0) {
     refs.message.innerHTML =
-      '<p>Ви ще не додали фільми до черги</p>';
+      '<p>Queue is empty</p>';
     refs.library.innerHTML = '';
     refs.pagination.style.display = 'none';
   } else if (queueMovies.length > 0) {
     refs.message.innerHTML = '';
      refs.pagination.style.display = 'flex';
     renderLibrary(queueMovies);
-   
   }
 }
 
