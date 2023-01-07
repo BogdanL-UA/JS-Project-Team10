f2cc17a2c8cd194ec5bed74343b346ec1b265a3f
rasengun <69122511+rasengun@users.noreply.github.com>
2023-01-07 20:58:17 +0200

    add login library

--- src/index-library.js
+++ src/index-library.js
@@ -2,6 +2,7 @@
 // import './js/create-search-gallery';
 import './js/open-movie-modal';
 import './js/pagination';
+import './js/authorisation';
 import './js/library/on-library-load';
 import './js/team-modal';
 import './js/dark-theme';
