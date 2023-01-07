d2552dbc6a8980d8be7f18a1f47ceb19fd1a1a66
rasengun <69122511+rasengun@users.noreply.github.com>
2023-01-07 21:14:59 +0200

    add spinner

--- src/js/create-page-home.js
+++ src/js/create-page-home.js
@@ -3,10 +3,9 @@ import { pagination } from './pagination';
 // import { showPagination } from './pagination';
 import { createMovieCard } from './get-trend-movies';
 import { FilmsApiService } from './api-service';
+import Loading from './spinner';
 
 import Pagination from 'tui-pagination';
-
- 
 
 const filmsApiService = new FilmsApiService();
 
@@ -15,11 +14,12 @@ window.addEventListener('load', uploadTrendMovies);
 //  async function uploadTrendMovies() {
 function uploadTrendMovies() {
   // showPagination();
-   filmsApiService.fetchTrendFilms().then(films => {
-     createMovieCard(films);
+  filmsApiService.fetchTrendFilms().then(films => {
+    Loading.pulse('Loading...', {
+      svgColor: '#FF6B08',
+    });
+    createMovieCard(films).finally(Loading.remove());
     //  showPagination();
   });
   pagination();
 }
-
-
